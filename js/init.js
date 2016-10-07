591b0e622a64aac9dae2571f97f84f59766e6cef
evgimov <e.ghimazitdinov>
2016-10-06 21:41:12 -0700

    Removed flexslider, deleted tweetie.min.js,added tweetie.js

--- js/init.js
+++ js/init.js
@@ -155,21 +155,6 @@ $('.tweet').twittie({
     });
 
 
-/*----------------------------------------------------*/
-/*	Flexslider
-/*----------------------------------------------------*/
-   $('.flexslider').flexslider({
-      namespace: "flex-",
-      controlsContainer: ".flex-container",
-      animation: 'slide',
-      controlNav: true,
-      directionNav: false,
-      smoothHeight: true,
-      slideshowSpeed: 7000,
-      animationSpeed: 600,
-      randomize: false,
-   });
-
 });
 
 
